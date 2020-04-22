c95ba0dab238742ed4c3edee6f432708a3090a52
Parthiv Chigurupati <2020pchiguru@tjhsst.edu>
2020-04-22 17:48:33 -0400

    First Commit

---
+++ Cloudflare 2020 Systems Project/cmake-build-debug/CMakeFiles/Cloudflare_2020_Systems_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cloudflare_2020_Systems_Project.dir/main.cpp.o"
+  "Cloudflare_2020_Systems_Project"
+  "Cloudflare_2020_Systems_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cloudflare_2020_Systems_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
